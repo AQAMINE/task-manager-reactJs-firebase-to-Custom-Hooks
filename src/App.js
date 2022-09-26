c0f214bd6cff66f1657acf2509c675ca60cd7847
amine aqebli <aqebli36@gmail.com>
2022-09-26 01:23:00 +0100

    hook optimization

--- src/App.js
+++ src/App.js
@@ -9,22 +9,22 @@ function App() {
   // const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = tasksObj => {
-    const loadedTasks = [];
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
+  
 
-    setTasks(loadedTasks);
-  };
-
-  const {isLoading, error, sendRequest: fetchTasks }  = useHttp({url: 'https://task-manager-to-custom-hooks-default-rtdb.firebaseio.com/tasks.json'}, 
-  transformTasks);
+  const {isLoading, error, sendRequest: fetchTasks }  = useHttp();
 
 //
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = tasksObj => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    };
+    fetchTasks({url: 'https://task-manager-to-custom-hooks-default-rtdb.firebaseio.com/tasks.json'},transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
